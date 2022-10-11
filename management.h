b168623d3466ef26effef78a6b145b91af78cbc9
sceria <97571051+sceria@users.noreply.github.com>
2022-10-10 17:05:58 -0700

    Create management.h

---
+++ management.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Defining the class
+class Animol {
+    public:
+    
+    // Class elements:
+    string ani; // Either a cat or dog
+    string nametag; // name of cat/dog
+    
+    // Methods:
+    
+};
+
+// Function defining:
+void add_animal();
+void adopt(int decision);
